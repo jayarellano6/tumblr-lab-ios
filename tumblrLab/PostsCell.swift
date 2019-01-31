128ad98dfe7cd76f98535feeb85d64988bbc649a
Jay Arellano <jayarellano@Jays-MacBook-Air.local>
2019-01-31 08:34:33 -0800

    finished tumblr lab

---
+++ tumblrLab/PostsCell.swift
@@ -0,0 +1,25 @@
+//
+//  postsCell.swift
+//  tumblrLab
+//
+//  Created by Jay Arellano on 1/30/19.
+//  Copyright © 2019 Jay Arellano. All rights reserved.
+//
+
+import UIKit
+
+class PostsCell: UITableViewCell {
+
+    @IBOutlet weak var postImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
